1f7cd2dd2580702663439c49a3b6cdd89942355b
CaioProg <caioprog18@gmail.com>
2023-02-02 11:01:03 -0300

    Creating Our POST Endpoint

--- TravelAgentWeb/Controllers/NotificationsController.cs
+++ TravelAgentWeb/Controllers/NotificationsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TravelAgentWeb.Data;
+using TravelAgentWeb.Dtos;
 
 namespace TravelAgentWeb.Controllers
 {
@@ -16,6 +17,32 @@ namespace TravelAgentWeb.Controllers
         public NotificationsController(TravelAgentDbContext context)
         {
             _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult FlightChanged(FlightDetailUpdateDto flightDetailUpdateDto)
+        {
+            Console.WriteLine($"Webhook Received from: {flightDetailUpdateDto.Publisher}");
+
+            var secretModel = _context.SubscriptionSecrets.FirstOrDefault(s => 
+            s.Publisher == flightDetailUpdateDto.Publisher && 
+            s.Secret == flightDetailUpdateDto.Secret);
+
+            if (secretModel == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Secret - Ignore Webhook");
+                Console.ResetColor();
+                return Ok();
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid Webhook!");
+                Console.WriteLine($"Old Preice {flightDetailUpdateDto.OldPrice}, New Price {flightDetailUpdateDto.NewPrice}");
+                Console.ResetColor();
+                return Ok();
+            }
         }
     }
 }
